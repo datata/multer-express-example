6ac03edb4f3f805b32f1858fcef5d4c99e4a331c
datata <dani@danitarazona.dev>
2024-04-05 12:20:43 +0200

    feat: serve static files

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import express from "express";
 
 const app = express();
 
+// Serve static files
+app.use(express.static('./src/public'))
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Healthy");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
